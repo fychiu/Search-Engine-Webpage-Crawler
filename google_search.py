a6960e92aabc3d0665953b8fd308e65d594c37b4
fychiu <b01611023@ntu.edu.tw>
2016-10-18 11:43:26 +0800

    google module example

---
+++ google_search.py
@@ -0,0 +1,14 @@
+#coding=utf8
+
+from google import search
+
+'''
+# Example:
+# more information is on the documentation of the google module websit
+
+res = search('Google Search', start=0, stop=20, pause=4.0)
+
+for url in res:
+    print url
+    
+'''
